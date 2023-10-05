3fa70676c3cec4bc7c744a64bb299632caf3110e
abbosko <agbosko@crimson.ua.edu>
2023-10-05 01:14:14 +0100

    my-app

---
+++ UAMBB/my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './AlabamaLogo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bamalytics
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
