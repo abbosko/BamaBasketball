63aa8c195d32c5e08ae0dcbcf4270a3664e7e4d2
snpierce <snpierce@crimson.ua.edu>
2023-10-08 22:10:48 -0500

    api call to get athlete info!

--- UAMBB/my-app/src/apiFunctions.js
+++ UAMBB/my-app/src/apiFunctions.js
@@ -1,6 +1,16 @@
 require('dotenv').config({path:"./.env"}); 
 const token = require('./genToken.js');
 
+
+// !!!!!!! MOVE LATER !!!!!!!!
+// Firstbeat athlete object
+function FBAthlete(fname, lname, email, id) {
+    this.fname = fname;
+    this.lname = lname;
+    this.email = email;
+    this.id = id;
+}
+
 /*const apiHawkinsTest = () => {
     fetch(process.env.HAWKINS_URL, {
         headers: {
@@ -55,9 +65,17 @@ apiHawkinsTest();*/
 
 apiKinexonTest();*/
 
-let fbAuth = 'Bearer ' + token.genFbToken();
 
-const apiFirstBeatTest = () => {
+
+/*
+* Firstbeat API calls! :)
+*/
+let fbAuth = 'Bearer ' + token.genFbToken();                // generates authorization token
+const fbAccountId = '3-4545';                                 // UA Men's Bball accountId
+var fbAthleteArray = [];
+
+// gets account info, stores account id
+/*const apiFirstBeatAccount = () => {
     fetch(process.env.FIRSTBEAT_URL, {
         headers: {
             Authorization: fbAuth,
@@ -66,20 +84,44 @@ const apiFirstBeatTest = () => {
     }) 
     .then(response => { 
         if (response.ok) { 
-        return response.text(); // Parse the response data as JSON 
-        //return response;
+            return response.json();
         } else { 
-        throw new Error('API request failed'); 
+            throw new Error('API request failed'); 
         } 
     }) 
-    .then(data => { 
-        // Process the response data here 
-        console.log(data); // Example: Logging the data to the console 
+    .then(data => {         
+        fbAccountId = data.accounts[0].accountId;           // firstbeat account id, need for all other API calls
+        console.log(fbAccountId);
     }) 
     .catch(error => { 
-        // Handle any errors here 
-        console.error(error); // Example: Logging the error to the console 
+        console.error(error);
+    });
+}*/
+
+
+// gets athlete info, creates instance of 'FBAthlete', stores in fbAthleteArray
+const apiFirstBeatAthletes = () => {
+    fetch((process.env.FIRSTBEAT_URL).concat('/', fbAccountId, '/athletes'), {  // add {accountId}/athletes to url
+        headers: {
+            Authorization: fbAuth,
+            "X-Api-Key": process.env.FIRSTBEAT_API_KEY
+        }
+    }) 
+    .then(response => { 
+        if (response.ok) { 
+            return response.json();
+        } else { 
+            throw new Error('API request failed'); 
+        } 
+    }) 
+    .then(data => {      
+        for(let i = 0; i < data.athletes.length; i++) {
+            fbAthleteArray.push(new FBAthlete(data.athletes[i].firstName, data.athletes[i].lastName, data.athletes[i].email, data.athletes[i].athleteId));
+        }
+    }) 
+    .catch(error => { 
+        console.error(error);
     });
 }
 
-apiFirstBeatTest();
+apiFirstBeatAthletes();
