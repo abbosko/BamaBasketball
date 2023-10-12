4c12713ecc64e889fc9c538346e1aaf9d076d12b
abbosko <agbosko@crimson.ua.edu>
2023-10-12 23:05:47 +0100

    removing py

--- UAMBB/bamalytics/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Player, Practice, Team, Statistics
-
-# Register your models here.
-admin.site.register(Player)
-admin.site.register(Practice)
-admin.site.register(Team)
-admin.site.register(Statistics)
