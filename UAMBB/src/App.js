4c3b9f53bef1b5fca1ca40200ce9e23f661f9191
abbosko <agbosko@crimson.ua.edu>
2023-10-26 20:01:21 +0100

    add front end

--- UAMBB/src/App.js
+++ UAMBB/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import React from 'react';
+import {teamLocker} from './screens/teamLocker';
+import {playerLocker} from './screens/playerLocker';
 import './App.css';
+import { Index } from './screens/Index';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Navbar />
+            <Routes>
+            <Route exact path='/' element={<Index />} />
+                <Route path='/teamLocker' element={<teamLocker />} />
+                <Route path='/playerLocker' element={<playerLocker />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ UAMBB/src/screens/playerLocker/playerLocker.js
@@ -0,0 +1,1 @@
+export { playerLocker } from "./playerLocker";
