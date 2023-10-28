cf31f2d5c24074ab1b6c6cad8cfdb2d9c9602015
snpierce <snpierce@crimson.ua.edu>
2023-10-27 22:05:41 -0500

    change colors

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -27,32 +27,21 @@ function Footer() {
       <Container fluid>
         <Nav>
           <NavItem>
-            <NavLink href="https://www.creative-tim.com/?ref=bdr-user-archive-footer">
-              Creative Tim
+            <NavLink href="https://alabama-mbb.access.kinexon.com">
+              Kinexon
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="https://www.creative-tim.com/presentation?ref=bdr-user-archive-footer">
-              About Us
+            <NavLink href="https://cloud.hawkindynamics.com/">
+              Hawkin Dynamics
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="https://www.creative-tim.com/blog?ref=bdr-user-archive-footer">
-              Blog
+            <NavLink href="https://sports.firstbeat.com/dashboard">
+              FirstBeat
             </NavLink>
           </NavItem>
         </Nav>
-        <div className="copyright">
-          © {new Date().getFullYear()} made with{" "}
-          <i className="tim-icons icon-heart-2" /> by{" "}
-          <a
-            href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
-            target="_blank"
-          >
-            Creative Tim
-          </a>{" "}
-          for a better web.
-        </div>
       </Container>
     </footer>
   );
